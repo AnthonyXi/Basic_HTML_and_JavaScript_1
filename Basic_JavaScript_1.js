ff3d1ad5da3957db7235e2fce85979faa7c0e4a7
AnthonyXi <xionganthony65609@gmail.com>
2025-05-29 17:22:30 -0700

    made changes for two parts assignment and challenges
    For a two-part assignment a

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+alert("Hello, World! I am an alert box!"); 
+	function My_First_Function() {
+		var String = "Marvel Rivals is a fun game!"
+		var result= String.fontcolor("blue");
+		document.getElementByid("Marvel Rivals").innerHTML =
+		result;
+	}
+	
